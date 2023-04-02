75a487a5feb571ba9c12651fb1c49eb4de41f126
fathimasilviyak <silviyakvm@gmail.com>
2023-04-02 21:17:35 +0530

    if the user's guessed state is in the us 50 states, then write it onto the map

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import turtle
+import pandas
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
 turtle.addshape(image)
 turtle.shape(image)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What is another state's name? ")
+answer_state = screen.textinput(title="Guess the State", prompt="What is another state's name? ").title()
+if answer_state in all_states:
+    new_state = turtle.Turtle()
+    new_state.hideturtle()
+    new_state.penup()
+    state_data = data[data.state == answer_state]
+    new_state.goto(int(state_data.x), int(state_data.y))
+    # new_state.write(state_data.state.item())
+    new_state.write(answer_state)
+
 
 # make the screen open always
 turtle.mainloop()
